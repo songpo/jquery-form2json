834e490460e3527e281121702df377ede583dbd5
刀之哀伤 <liusongpo@gmail.com>
2015-01-25 17:27:33 +0800

    提交测试代码

--- js/form2json/1.0/form2json.js
+++ js/form2json/1.0/form2json.js
@@ -1,12 +1,3 @@
-/*!
- * formutil
- * 一个jqeury扩展，可以将复杂表单序列化成JSON串
- *
- * @version     v1.0, built on 2015-01-25 16:48:03 AM
- * @author      https://twitter.com/nghuuphuoc
- * @copyright   (c) 2013 - 2015 songpo
- * @license     GPL还是什么的都没有，随便使用
- */
 $.fn.form2json = function(){
     var obj = {};
     $.each(this.serializeArray(), function(index, item){
